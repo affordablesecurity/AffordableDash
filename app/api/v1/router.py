97f889bf2133cb2498beb5c83c08810857ca14ca
affordablesecurity <brandon@callaffordablesecurity.com>
2026-02-07 17:00:22 -0700

    Update router.py

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,9 @@
-from __future__ import annotations
-
-from fastapi import APIRouter
+ffrom fastapi import APIRouter
 
 from app.api.v1.endpoints import auth
+from app.api.v1.endpoints import customers
 
 api_router = APIRouter()
+
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(customers.router, prefix="/customers", tags=["customers"])
